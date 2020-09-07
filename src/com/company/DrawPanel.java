986610f6dc2ec1dfa407170d7b6e745597cd838a
Lesya Lipatova <lesya-lipatova01@mail.ru>
2020-09-07 19:37:47 +0300

    Сделала заливку носа

--- src/com/company/DrawPanel.java
+++ src/com/company/DrawPanel.java
@@ -8,9 +8,6 @@ public class DrawPanel extends JPanel {
     public void paint(Graphics g) {
         Graphics2D gr = (Graphics2D) g;
         gr.setColor(Color.blue);
-        // gr.drawLine(100, 100, 100, 100);
-        // drawSun(gr, 100, 100, 20, 20, 100, Color.blue);
-        // gr.fillOval(15, 15, 15, 15);
         Color lightBlue = new Color(150, 150, 255);
         drawBackground(gr, getWidth(), getHeight(), lightBlue, Color.LIGHT_GRAY);
         drawSnowman(gr, getWidth() / 2, getHeight() / 2);
@@ -48,32 +45,10 @@ public class DrawPanel extends JPanel {
         g.fillOval(x1 + 20, y1 - 20, r, r);
 
         g.setColor(nose);
-        g.drawLine(x, y + 5, x, y + 20);
-        g.drawLine(x, y + 5, x - 25, y + 12);
-        g.drawLine(x, y + 20, x - 25, y + 12);
-
+        Polygon p = new Polygon();
+        p.addPoint(x, y + 5);
+        p.addPoint(x - 25, y + 12);
+        p.addPoint(x, y + 20);
+        g.fillPolygon(p);
     }
-
-    public void drawArm(Graphics2D g, int x1, int y1, int x2, int y2) {
-
-    }
-
-
-    public static void drawSun(Graphics g, int x, int y, int r, int R, int n, Color c) {
-        g.setColor(c);
-        g.fillOval(x - r, y - r, r + r, r + r);
-        double da = 2 * Math.PI / n;
-        for (int i = 0; i < n; i++) {
-            double dx1 = r * Math.cos(da * i);
-            double dy1 = r * Math.sin(da * i);
-            double dx2 = R * Math.cos(da * i);
-            double dy2 = R * Math.sin(da * i);
-            g.drawLine(x + (int) dx1, y + (int) dy1, x + (int) dx2, x + (int) dy2);
-        }
-    }
-
-   /* public static void drawKub (Graphics g, int x, int y, int x2, int y2, Color c) {
-        g.setColor(c);
-
-    }*/
 }
